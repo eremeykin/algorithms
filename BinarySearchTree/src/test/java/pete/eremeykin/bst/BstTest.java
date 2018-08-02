09f8cdaabc3e5f6cc88a8bebf736c012638d1f59
Petr Eremeykin <eremeykin@gmail.com>
2018-08-02 15:54:34 +0300

    Tests refactoring

--- BinarySearchTree/src/test/java/pete/eremeykin/bst/BstTest.java
+++ BinarySearchTree/src/test/java/pete/eremeykin/bst/BstTest.java
@@ -2,20 +2,89 @@ package pete.eremeykin.bst;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
-import static pete.eremeykin.bst.test.mathers.BstSizeMatcher.hasSize;
-import static pete.eremeykin.bst.test.random.IntegerArgumentGenerator.fromExisting;
 
+import java.util.EnumMap;
 import java.util.Random;
+import java.util.TreeMap;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import static pete.eremeykin.bst.test.mathers.BstSizeMatcher.*;
+import pete.eremeykin.bst.test.framework.Methods;
+import pete.eremeykin.bst.test.framework.SynchronousBstModifier;
+import pete.eremeykin.bst.test.framework.TreeMapToBstAdapter;
 import pete.eremeykin.bst.test.random.ArgumentGenerator;
 import pete.eremeykin.bst.test.random.IntegerArgumentGenerator;
+import pete.eremeykin.bst.test.random.RandomOperation;
 import pete.eremeykin.bst.test.random.StringArgumentGenerator;
+import static pete.eremeykin.bst.test.random.IntegerArgumentGenerator.*;
 
-public class BstIntegerStringTest extends BstBaseTest<Integer, String> {
+public class BstTest{
 
+	private static Random random = new Random(34456432L);
+	Bst<Integer, String> bst;
+	Bst<Integer, String> underTestBst;
+	Bst<Integer, String> robustBST;
+	SynchronousBstModifier<Integer, String> sbModifier;
+
+	@Before
+	public void setUp() {
+		underTestBst = new BstImpl<Integer, String>();
+		robustBST = new TreeMapToBstAdapter<Integer, String>(new TreeMap<Integer, String>());
+		sbModifier = new SynchronousBstModifier<Integer, String>(underTestBst, robustBST);
+	}
+
+	private static EnumMap<Methods, Double> getMap() {
+		EnumMap<Methods, Double> map = new EnumMap<Methods, Double>(Methods.class);
+		map.put(Methods.PUT, 0.4);
+		map.put(Methods.DELETE, 0.1);
+		map.put(Methods.DELETE_MIN, 0.1);
+		map.put(Methods.MIN, 0.1);
+		map.put(Methods.GET, 0.3);
+		return map;
+	}
+
+	@Test
+	public void testSize() {
+		int iterations = random.nextInt(3400) + 100;
+		for (int c = 0; c < iterations; c++) {
+			int operationsNumber = random.nextInt(500);
+			for (int i = 0; i < operationsNumber; i++) {
+				Methods method = RandomOperation.generate(getMap());
+				switch (method) {
+				case PUT:
+					put();
+					break;
+				case DELETE:
+					delete();
+					break;
+				case DELETE_MIN:
+					deleteMin();
+					break;
+				case GET:
+					get();
+					break;
+				case MIN:
+					min();
+					break;
+				case FLOOR:
+					floor();
+					break;
+				case RANK:
+					rank();
+					break;
+				case SELECT:
+					select();
+					break;
+				}
+			}
+		}
+		Integer actualSize = robustBST.size();
+		assertThat(underTestBst, hasSize(actualSize));
+	};
+	
 	private ArgumentGenerator<String> stringGenerator = new StringArgumentGenerator(0.75);
 	private ArgumentGenerator<Integer> integerGenerator = new IntegerArgumentGenerator(0.75);
 	private ArgumentGenerator<Integer> hFreqIntegerGenerator = fromExisting(integerGenerator, 0.05);
@@ -34,7 +103,6 @@ public class BstIntegerStringTest extends BstBaseTest<Integer, String> {
 		assertThat(sbModifier.getReal().get(220), equalTo(sbModifier.getRobust().get(220)));
 	}
 
-	@Override
 	protected void put() {
 		Integer key = integerGenerator.generate();
 		String value = stringGenerator.generate();
@@ -42,14 +110,12 @@ public class BstIntegerStringTest extends BstBaseTest<Integer, String> {
 		this.sbModifier.put(key, value);
 	}
 
-	@Override
 	protected void delete() {
 		Integer key = integerGenerator.generate();
 		System.out.println(String.format("delete(%s)", key));
 		this.sbModifier.delete(key);
 	}
 
-	@Override
 	protected void deleteMin() {
 		Integer actual = sbModifier.getReal().min();
 		Integer expected = sbModifier.getRobust().min();
@@ -63,7 +129,6 @@ public class BstIntegerStringTest extends BstBaseTest<Integer, String> {
 		assertThat(sbModifier.getReal(), hasSize(sbModifier.getRobust().size()));
 	}
 
-	@Override
 	protected void get() {
 		Integer key = hFreqIntegerGenerator.generate();
 		String actualValue = this.sbModifier.get(key);
@@ -73,7 +138,6 @@ public class BstIntegerStringTest extends BstBaseTest<Integer, String> {
 		Assert.assertThat(actualValue, equalTo(expectedValue));
 	}
 
-	@Override
 	protected void min() {
 		Integer actual = sbModifier.getReal().min();
 		Integer expected = sbModifier.getRobust().min();
@@ -82,7 +146,6 @@ public class BstIntegerStringTest extends BstBaseTest<Integer, String> {
 
 	}
 
-	@Override
 	protected void floor() {
 		Integer key = new Random(6345L).nextInt(100000);
 		Integer actual = sbModifier.getReal().floor(key);
@@ -91,7 +154,6 @@ public class BstIntegerStringTest extends BstBaseTest<Integer, String> {
 		assertThat(actual, equalTo(expected));
 	}
 
-	@Override
 	protected void select() {
 		Integer rank = integerGenerator.generate();
 		Integer actual = sbModifier.getReal().select(rank);
@@ -101,7 +163,6 @@ public class BstIntegerStringTest extends BstBaseTest<Integer, String> {
 		assertThat(actual, equalTo(expected));
 	}
 
-	@Override
 	protected void rank() {
 		Integer key = hFreqIntegerGenerator.generate();
 		Integer actual = sbModifier.getReal().rank(key);
@@ -111,5 +172,6 @@ public class BstIntegerStringTest extends BstBaseTest<Integer, String> {
 		assertThat(actual, equalTo(expected));
 
 	}
+
 
 }
